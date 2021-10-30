f5c8900d9785f2e0a781b7296cfee662e1c78126
stasso96 <stasso96@yahoo.com>
2021-10-30 19:27:06 -0400

    added error handling

--- Programming_Assignment_Tasso_v2.py
+++ Programming_Assignment_Tasso_v2.py
@@ -4,11 +4,12 @@ import csv
 from pathlib import Path
 
 class Binance :
-    def __init__(self, filename):
+    def __init__(self, filename, symbol):
         self.filename = filename
+        self.symbol = symbol
     #-------------------------------------------------------------------------------------------------------------
     #pulls candlestick data from Binance.us/api/v3/klines
-    def get_data(self, symbol, start_time) :
+    def get_data(self, start_time) :
         API_KEY = '8Ji91hqp6yuLbnRwKPFWQB8aUyY5SeuhJ5TbjQXVDNp40mVoR9dnEk8ybfI2r70T'
         j = []
         params = {
@@ -21,12 +22,16 @@ class Binance :
         headers = {
         'X-MBX-APIKEY': API_KEY
         }
-        params['symbol'] = symbol
+        params['symbol'] = self.symbol
         params['startTime'] = start_time 
+        
 
         while(True) :
             r = requests.get('https://api.binance.us/api/v3/klines', params=params, headers=headers)
+            if r.status_code != 200:
+                return 1
             j += r.json()
+
             params["startTime"] = j[-1][0]
             
             if(str(j[-1][0]) == self.get_15_before_midnight()) :
@@ -38,11 +43,15 @@ class Binance :
 
     #-------------------------------------------------------------------------------------------------------------
     #creates a csv file and Write the candlestick data to the file
-    def create_csv(self, symbol, filename) :
-        print(f"creating {filename}...")
-        data = self.get_data(symbol, self.get_first_of_month_timestamp())
+    def create_csv(self) :
+        print(f"creating {self.filename}...")
+        data = self.get_data(self.get_first_of_month_timestamp())
+        
+        if(data == 1) :
+            print("ERROR: Unable to fetch data!")
+            return
 
-        with open(filename, "w", newline='') as f:
+        with open(self.filename, "w", newline='') as f:
             writer = csv.writer(f)
             writer.writerows(data)
 
@@ -50,21 +59,26 @@ class Binance :
 
     #-------------------------------------------------------------------------------------------------------------
     #checks to see if the .csv file has the most recent data, if it does not, appends the new data to the .csv.
-    def append_to_csv(self, symbol, filename) :
-        print(f"adding new data to {filename}...")
-        file = open(filename, "r")
+    def append_to_csv(self) :
+        print(f"adding new data to {self.filename}...")
+        file = open(self.filename, "r")
         input = file.readlines()
         last_date_csv = input[-1].partition(',')[0]
         last_date_obj = dt.datetime.fromtimestamp(float(last_date_csv) / 1000) 
         yesterday_obj = dt.datetime.fromtimestamp(float(self.get_yesterday_timestamp()) / 1000)
 
         if(last_date_obj.date() == yesterday_obj.date()) :
-            print(f"{filename} is already up to date\nDone.")
+            print(f"{self.filename} is already up to date\nDone.")
             return 
 
-        data = self.get_data(symbol, last_date_csv)
+        data = self.get_data(last_date_csv)
+
+        if(data == 1) :
+            print("ERROR: Unable to fetch data!")
+            return
+
         data.pop(0)
-        file = open(filename, "a", newline='')
+        file = open(self.filename, "a", newline='')
         writer = csv.writer(file)
         writer.writerows(data)
         print(f"Done.")  
@@ -113,34 +127,33 @@ class Bitcoin(Binance):
     SYMBOL = 'BTCUSDT'
 
     def __init__(self, filename):
-        super().__init__(filename) 
+        super().__init__(filename, self.SYMBOL) 
 
 class Ethereum(Binance):
     SYMBOL = 'ETHUSDT'
 
     def __init__(self, filename):
-        super().__init__(filename) 
+        super().__init__(filename, self.SYMBOL) 
 
 
 btc_filename = 'btcv2.csv'
 eth_filename = 'ethv2.csv'
-
-bitcoin = Bitcoin('btcv2.csv')
-ethereum = Ethereum('ethv2.csv')
+bitcoin = Bitcoin(btc_filename)
+ethereum = Ethereum(eth_filename)
 
 if Path(btc_filename).is_file():
     print ("Bitcoin File exists.")
-    bitcoin.append_to_csv('BTCUSDT', btc_filename,)
+    bitcoin.append_to_csv()
     
 else:
     print ("Bitcoin File does not exist.")
-    bitcoin.create_csv('BTCUSDT', btc_filename,)
+    bitcoin.create_csv()
 
 if Path(eth_filename).is_file():
     print ("Etherium File exists.")
-    ethereum.append_to_csv('ETHUSDT', eth_filename,)
+    ethereum.append_to_csv()
 
 else:
     print ("Etherium File does not exist.")
-    ethereum.create_csv('ETHUSDT', eth_filename,)
+    ethereum.create_csv()
 
